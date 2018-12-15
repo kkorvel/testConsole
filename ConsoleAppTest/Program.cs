d2d8c784c5a2682a418c5822c31befd7470a7200
Koit Kõrvel <koit.korvel@gmail.com>
2018-12-15 19:55:41 +0200

    correct

---
+++ ConsoleAppTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> peoples = new List<string>();
+
+            
+            Person person = new Person();
+            Console.WriteLine("Tere, mis on su nimi?");
+
+            person.name = Console.ReadLine();
+
+            Console.WriteLine("Tere," + person.name + " kui vana sa oled?");
+
+            person.age = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Meeldiv tutvuda, " + person.name + " sa oled " + person.age + " aastane!");
+
+           
+            peoples.Add(person.name);
+            peoples.Add(person.age.ToString());
+
+
+            Console.WriteLine("Peoples:");
+            foreach (string item in peoples)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+        
+    }
+}
